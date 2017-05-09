c7165d373a3b2dcf46ca2b3c3176870534fae99b
deka <deka@users.noreply.github.com>
2017-05-09 18:08:52 +0200

    v 1.04

--- RemoteRetentionCleaner.ps1
+++ RemoteRetentionCleaner.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 param (
 )
 
@@ -10,22 +10,28 @@ Trace-VstsEnteringInvocation $MyInvocation
 [string]$remoteComputer = Get-VstsInput -Name "remoteComputer" -Require
 [string]$userName = Get-VstsInput -Name "userName" -Require
 [string]$password = Get-VstsInput -Name "password" -Require
+[string]$excludes = (Get-VstsInput -Name "excludes").Trim().Replace("`n",",")
 
+Write-Host "Version 1.0.4"
+Write-Host "remoteComputer = $remoteComputer"
 Write-Host "folderPath = $folderPath"
 Write-Host "retentionDays = $retentionDays"
 Write-Host "minimumToKeep = $minimumToKeep"
-Write-Host "remoteComputer = $remoteComputer"
 Write-Host "userName = $userName"
 Write-Host "password = $password"
+Write-Host "excludes = $excludes"
 
 $script = { 
 		  
 			 $folderPath = $args[0]
 			 $retentionDays = $args[1]
 			 $minimumToKeep = $args[2]
+			 $excludes = $args[3]
 
-	$folderlist = Get-ChildItem $folderPath | sort @{Expression={$_.LastWriteTime}; Ascending=$false}
-
+	$folderlist = Get-ChildItem $folderPath -Exclude $excludes.Split(",") | sort @{Expression={$_.LastWriteTime}; Ascending=$false}
+	Write-Host "Item list : "
+	Write-Host $folderlist
+	
 	$keepDate = (Get-Date).AddDays(-$retentionDays)
 	$keeping = 0
     $deleting = 0
@@ -54,12 +60,19 @@ if($computers.Count -eq 0)
    
 foreach($computer in $computers)
 {
-	Write-Host "Start process. Computer nam : $($computer)."
+	Write-Host "Start process. Computer name : $($computer)."
+	Write-Host "	- PSCredential"
 	$credential = New-Object System.Management.Automation.PSCredential($userName, (ConvertTo-SecureString -String $password -AsPlainText -Force));
+	Write-Host "	- Initialize New-PSSession"
 	$session = New-PSSession -ComputerName $computer -Credential $credential;
-	Invoke-Command -Session $session -ScriptBlock $script -ArgumentList $folderPath, $retentionDays ,$minimumToKeep
+	Write-Host "	- Invoke-Command"
+	Invoke-Command -Session $session -ScriptBlock $script -ArgumentList $folderPath, $retentionDays ,$minimumToKeep, $excludes
+	Write-Host "	- Remove-PSSession"
 	Remove-PSSession -Session $session
 }
+
+
+
 
 
 
